feae0bc7ae4ccdab8b9a3dc5a6e8d70e4a48c676
OWAIS <120783442+owais-alhjri@users.noreply.github.com>
2025-11-02 10:34:58 +0400

    Add .gitignore

---
+++ src/test/java/com/example/curdoprations/CurdOprationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curdoprations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOprationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
