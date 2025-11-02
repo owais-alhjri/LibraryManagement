29f84087edd093ec44aaefd599520bdc1794ae24
OWAIS <120783442+owais-alhjri@users.noreply.github.com>
2025-11-02 10:38:35 +0400

    starting the project

--- src/main/java/com/example/curdoprations/CurdOprationsApplication.java
+++ src/main/java/com/example/curdoprations/CurdOprationsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurdOprationsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurdOprationsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CurdOprationsApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/curdoprations/controllers/curdController.java
+++ src/main/java/com/example/curdoprations/controllers/curdController.java
@@ -1,4 +1,7 @@
 package com.example.curdoprations.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class curdController {
 }
